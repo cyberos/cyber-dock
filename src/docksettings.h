e39d68ca400c291b174056c25d8796e435287b3e
rekols <revenmartin@gmail.com>
2020-10-23 15:23:56 +0800

    DarkMode interface using MeuiKit

--- src/docksettings.h
+++ src/docksettings.h
@@ -8,7 +8,6 @@
 class DockSettings : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(bool darkMode READ darkMode NOTIFY darkModeChanged)
 
 public:
     enum Direction {
@@ -20,24 +19,12 @@ public:
     static DockSettings *self();
     explicit DockSettings(QObject *parent = nullptr);
 
-    bool darkMode() { return m_darkMode; }
-    void setDarkMode(bool enable);
-
     void setDirection(Direction direction);
 
-private slots:
-    void onDBusDarkModeChanged(bool darkMode);
-
-private:
-    void initDBusSignals();
-    void initData();
-
 Q_SIGNALS:
-    void darkModeChanged();
     void directionChanged();
 
 private:
-    bool m_darkMode;
     Direction m_direction;
     QSettings *m_settings;
     QFileSystemWatcher *m_fileWatcher;
