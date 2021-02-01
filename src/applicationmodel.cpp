c07b2d45c31833916cee0011a80e801563766529
rekols <revenmartin@gmail.com>
2021-02-01 14:11:26 +0800

    Reset active index on remove

--- src/applicationmodel.cpp
+++ src/applicationmodel.cpp
@@ -1,4 +1,23 @@
-﻿#include "applicationmodel.h"
+﻿/*
+ * Copyright (C) 2020 ~ 2021 CyberOS Team.
+ *
+ * Author:     rekols <revenmartin@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "applicationmodel.h"
 #include "utils.h"
 
 #include <QProcess>
@@ -357,6 +376,10 @@ void ApplicationModel::onWindowRemoved(quint64 wid)
 
     // Remove from wid list.
     item->wids.removeOne(wid);
+
+    if (item->currentActive >= item->wids.size())
+        item->currentActive = 0;
+
     handleDataChangedFromItem(item);
 
     if (item->wids.isEmpty()) {
