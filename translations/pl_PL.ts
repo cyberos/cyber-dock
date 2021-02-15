2a864f94f4a5ce4f5f36678f8dde16e9b1f22b49
omame <me@omame.tech>
2021-02-15 19:30:15 +0100

    Add Polish translation

---
+++ translations/pl_PL.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL" sourcelanguage="en">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="50"/>
+        <source>Open</source>
+        <translation>Otwórz</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Unpin</source>
+        <translation>Odepnij</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Pin</source>
+        <translation>Przypnij</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="69"/>
+        <source>Close All</source>
+        <translation>Zamknij wszystkie</translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="167"/>
+        <source>Wi-Fi</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <source>On</source>
+        <translation>Włączone</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <location filename="../qml/ControlCenter.qml" line="181"/>
+        <source>Off</source>
+        <translation>Wyłączone</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="180"/>
+        <source>Bluetooth</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="125"/>
+        <source>Launcher</source>
+        <translatorcomment>There&apos;s no translation, the Polish equivalent would be &quot;wyrzutnia&quot; but it doesn&apos;t fit.</translatorcomment>
+        <translation></translation>
+    </message>
+</context>
+</TS>
