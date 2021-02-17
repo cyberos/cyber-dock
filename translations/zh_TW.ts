3944e12edffd63009b725f521e93c42a498bb467
windowsboy111 <wboy111@outlook.com>
2021-02-17 10:49:02 +0800

    added zh_HK and zh_TW translations

---
+++ translations/zh_TW.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_TW">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="50"/>
+        <source>Open</source>
+        <translation>開啟</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Unpin</source>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Pin</source>
+        <translation>固定</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="69"/>
+        <source>Close All</source>
+        <translation>全部關閉</translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="167"/>
+        <source>Wi-Fi</source>
+        <translation>Wi-Fi</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <source>On</source>
+        <translation>開</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <location filename="../qml/ControlCenter.qml" line="181"/>
+        <source>Off</source>
+        <translation>關</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="180"/>
+        <source>Bluetooth</source>
+        <translation>藍芽</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="125"/>
+        <source>Launcher</source>
+        <translation>程式啟動器</translation>
+    </message>
+</context>
+</TS>
