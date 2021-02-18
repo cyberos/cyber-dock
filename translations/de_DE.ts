e8c15ecbbae25f084ffd9683afd6b9ec450c64c0
theemutvcode <55019667+theemutvcode@users.noreply.github.com>
2021-02-18 15:15:42 +0100

    add german translation (#18)

---
+++ translations/de_DE.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="de_DE">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="50"/>
+        <source>Open</source>
+        <translation>Öffnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Unpin</source>
+        <translation>Entpinnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Pin</source>
+        <translation>Anpinnen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="69"/>
+        <source>Close All</source>
+        <translation>Alles schließen</translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="167"/>
+        <source>Wi-Fi</source>
+        <translation>WLAN</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <source>On</source>
+        <translation>An</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <location filename="../qml/ControlCenter.qml" line="181"/>
+        <source>Off</source>
+        <translation>Aus</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="180"/>
+        <source>Bluetooth</source>
+        <translation>Bluetooth</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="125"/>
+        <source>Launcher</source>
+        <translation>Launcher</translation>
+    </message>
+</context>
+</TS>
