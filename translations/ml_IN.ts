423951ccfe5ace06f8f42b03df3ee66af369c748
Anonymous <noreply@weblate.org>
2021-05-05 07:36:02 +0000

    Translated using Weblate (Malayalam)
    Currently translated at 0.0% (0 of 9 strings)
    
    Translation: CyberOS/Dock
    Translate-URL: https://hosted.weblate.org/projects/cyberos/dock/ml/

--- translations/ml_IN.ts
+++ translations/ml_IN.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/AppItem.qml" line="50"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">തുറക്കുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Unpin</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">അൺപിൻ ചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Pin</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">പിൻ ചെയ്യുക</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="69"/>
         <source>Close All</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">എല്ലാം അടയ്‌ക്കുക</translation>
     </message>
 </context>
 <context>
@@ -29,23 +29,23 @@
     <message>
         <location filename="../qml/ControlCenter.qml" line="167"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">വൈഫൈ</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <source>On</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ഓണാണ്</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <location filename="../qml/ControlCenter.qml" line="181"/>
         <source>Off</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ഓഫാണ്</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="180"/>
         <source>Bluetooth</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ബ്ലൂടൂത്ത്</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../qml/main.qml" line="125"/>
         <source>Launcher</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">ലോഞ്ചർ</translation>
     </message>
 </context>
 </TS>
