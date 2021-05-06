10d3e70d4d4fd4e0c003467dbabe5fca7f9d9876
wolfemium <stefan@grafician.com>
2021-05-06 16:01:31 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: CyberOS/Dock
    Translate-URL: https://hosted.weblate.org/projects/cyberos/dock/ro/

--- translations/ro_RO.ts
+++ translations/ro_RO.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/AppItem.qml" line="50"/>
         <source>Open</source>
-        <translation type="unfinished">Deschideți</translation>
+        <translation>Deschideți</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Unpin</source>
-        <translation type="unfinished">Deșurubați</translation>
+        <translation>Defixați</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Pin</source>
-        <translation type="unfinished">Pin</translation>
+        <translation>Fixați</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="69"/>
         <source>Close All</source>
-        <translation type="unfinished">Închideți toate</translation>
+        <translation>Închideți toate</translation>
     </message>
 </context>
 <context>
@@ -29,23 +29,23 @@
     <message>
         <location filename="../qml/ControlCenter.qml" line="167"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished">Wi-Fi</translation>
+        <translation>Wi-Fi</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <source>On</source>
-        <translation type="unfinished">Pe</translation>
+        <translation>Pornit</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <location filename="../qml/ControlCenter.qml" line="181"/>
         <source>Off</source>
-        <translation type="unfinished">Off</translation>
+        <translation>Oprit</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="180"/>
         <source>Bluetooth</source>
-        <translation type="unfinished">Bluetooth</translation>
+        <translation>Bluetooth</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../qml/main.qml" line="125"/>
         <source>Launcher</source>
-        <translation type="unfinished">Lansator</translation>
+        <translation>Lansator</translation>
     </message>
 </context>
 </TS>
