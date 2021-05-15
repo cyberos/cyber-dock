bee7c6f400f8bfd31fc916f3694c03f26836ed76
SilvinSooraj <silvanosnelli@gmail.com>
2021-05-15 17:32:02 +0000

    Translated using Weblate (Hindi)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: CyberOS/Dock
    Translate-URL: https://hosted.weblate.org/projects/cyberos/dock/hi/

--- translations/hi_IN.ts
+++ translations/hi_IN.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../qml/AppItem.qml" line="50"/>
         <source>Open</source>
-        <translation type="unfinished">खुला हुआ</translation>
+        <translation>खुलो</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Unpin</source>
-        <translation type="unfinished">अनपिन</translation>
+        <translation>अनपिन</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="62"/>
         <source>Pin</source>
-        <translation type="unfinished">पिन</translation>
+        <translation>पिन</translation>
     </message>
     <message>
         <location filename="../qml/AppItem.qml" line="69"/>
         <source>Close All</source>
-        <translation type="unfinished">सब बंद करें</translation>
+        <translation>सब बंद करें</translation>
     </message>
 </context>
 <context>
@@ -29,23 +29,23 @@
     <message>
         <location filename="../qml/ControlCenter.qml" line="167"/>
         <source>Wi-Fi</source>
-        <translation type="unfinished">वाई - फाई</translation>
+        <translation>वाई - फाई</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <source>On</source>
-        <translation type="unfinished">पर</translation>
+        <translation>ऑन</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="170"/>
         <location filename="../qml/ControlCenter.qml" line="181"/>
         <source>Off</source>
-        <translation type="unfinished">बंद</translation>
+        <translation>ऑफ</translation>
     </message>
     <message>
         <location filename="../qml/ControlCenter.qml" line="180"/>
         <source>Bluetooth</source>
-        <translation type="unfinished">ब्लूटूथ</translation>
+        <translation>ब्लूटूथ</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,7 @@
     <message>
         <location filename="../qml/main.qml" line="125"/>
         <source>Launcher</source>
-        <translation type="unfinished">लांचर</translation>
+        <translation>लांचर</translation>
     </message>
 </context>
 </TS>
