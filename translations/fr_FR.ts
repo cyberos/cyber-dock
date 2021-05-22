fd90da072fa4a2e9ea7e8f8160b7ac989b3ec801
Weblate <noreply@weblate.org>
2021-05-22 16:13:33 +0200

    Added translation using Weblate (French)

---
+++ translations/fr_FR.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="de_DE">
+<context>
+    <name>AppItem</name>
+    <message>
+        <location filename="../qml/AppItem.qml" line="50"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Unpin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="62"/>
+        <source>Pin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AppItem.qml" line="69"/>
+        <source>Close All</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="167"/>
+        <source>Wi-Fi</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="170"/>
+        <location filename="../qml/ControlCenter.qml" line="181"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlCenter.qml" line="180"/>
+        <source>Bluetooth</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="125"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
